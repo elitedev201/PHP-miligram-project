b72f65eda98a7d8f0ee5edcfa2ed7494d452d707
DevKing201 <hfmlarisa.pyr1989ld313jzc0@gmail.com>
2021-09-16 12:53:18 -0700

    first commit about login and sign up page

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  $("#login").click(function () {
+    $(".mask").css("display", "block")
+    $(".modal-login").css("display", "block")
+  })
+
+  $(".close-button").click(function () {
+    $(".modal-login").css("display", "none")
+    $(".modal-signup").css("display", "none")
+    $(".mask").css("display", "none")
+  })
+
+  $("#sign_btn").click(function () {
+    $(".modal-login").css("display", "none")
+    $(".modal-signup").css("display", "block")
+  })
+
+  $("#register").click(function () {
+    $(".mask").css("display", "block")
+    $(".modal-signup").css("display", "block")
+  })
+
+  $(document).keyup(function (e) {
+    if (e.key === "Escape") {
+      $(".modal-login").css("display", "none")
+      $(".mask").css("display", "none")
+      $(".modal-signup").css("display", "none")
+    }
+  })
+
+  $(".mask").click(function () {})
+})
